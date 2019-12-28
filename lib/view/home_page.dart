26d70e7f4e271fca4dca4b96e63ca80b7f78afb8
Daniel Marmelo <daniel.marmelo@hotmail.com>
2019-12-28 13:34:14 +0000

    Limpeza do código da home page. Navigator da pesquisa a ir para a página de pesquisa. Adição do chip field na página de pesquisa.

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,14 +1,10 @@
-import 'dart:async';
-
-import 'package:cow_manager/model/animal.dart';
 import 'package:cow_manager/services/authentication.dart';
 import 'package:cow_manager/view/new_animal.dart';
+import 'package:cow_manager/view/search_animal.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_database/firebase_database.dart';
 
 class HomePage extends StatefulWidget {
-  HomePage({Key key, this.auth, this.userId, this.logoutCallback})
-      : super(key: key);
+  HomePage({Key key, this.auth, this.userId, this.logoutCallback}) : super(key: key);
 
   final BaseAuth auth;
   final VoidCallback logoutCallback;
@@ -16,52 +12,10 @@ class HomePage extends StatefulWidget {
 
   @override
   State<StatefulWidget> createState() => new _HomePageState();
+
 }
 
 class _HomePageState extends State<HomePage> {
-  final FirebaseDatabase _database = FirebaseDatabase.instance;
-
-  final _textEditingController = TextEditingController();
-
-  List<Animal> _animalList;
-  Query _animalQuery;
-  StreamSubscription<Event> _onAnimalAddedSubscription;
-  StreamSubscription<Event> _onAnimalChangedSubscription;
-
-  @override
-  void initState() {
-    super.initState();
-
-    _animalList = new List();
-    _animalQuery = _database.reference().child("animais");
-    _onAnimalAddedSubscription = _animalQuery.onChildAdded.listen(onEntryAdded);
-    _onAnimalChangedSubscription =
-        _animalQuery.onChildChanged.listen(onEntryChanged);
-  }
-
-  @override
-  void dispose() {
-    _onAnimalAddedSubscription.cancel();
-    _onAnimalChangedSubscription.cancel();
-    super.dispose();
-  }
-
-  onEntryChanged(Event event) {
-    var oldEntry = _animalList.singleWhere((entry) {
-      return entry.key == event.snapshot.key;
-    });
-
-    setState(() {
-      _animalList[_animalList.indexOf(oldEntry)] =
-          Animal.fromSnapshot(event.snapshot);
-    });
-  }
-
-  onEntryAdded(Event event) {
-    setState(() {
-      _animalList.add(Animal.fromSnapshot(event.snapshot));
-    });
-  }
 
   signOut() async {
     try {
@@ -72,97 +26,6 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
-  showAddAnimalDialog(BuildContext context) async {
-    _textEditingController.clear();
-    await showDialog<String>(
-        context: context,
-        builder: (BuildContext context) {
-          return AlertDialog(
-            content: new Row(
-              children: <Widget>[
-                new Expanded(
-                    child: new TextField(
-                  controller: _textEditingController,
-                  autofocus: true,
-                  decoration: new InputDecoration(
-                    labelText: 'Add new todo',
-                  ),
-                ))
-              ],
-            ),
-            actions: <Widget>[
-              new FlatButton(
-                  child: const Text('Cancel'),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  }),
-              new FlatButton(
-                  child: const Text('Save'),
-                  onPressed: () {
-                    //addNewAnimal(_textEditingController.text.toString());
-                    Navigator.pop(context);
-                  })
-            ],
-          );
-        });
-  }
-/*
-  Widget showAnimalList() {
-    if (_animalList.length > 0) {
-      return ListView.builder(
-          shrinkWrap: true,
-          itemCount: _animalList.length,
-          itemBuilder: (BuildContext context, int index) {
-            String animalId = _animalList[index].key;
-            String electronicId = _animalList[index].electronicId;
-            String earring = _animalList[index].earring;
-            String userId = _animalList[index].userId;
-            return Dismissible(
-              key: Key(animalId),
-              background: Container(color: Colors.red),
-              onDismissed: (direction) async {
-                //deleteTodo(animalId, index);
-              },
-              child: ListTile(
-                title: Text(
-                  earring,
-                  style: TextStyle(fontSize: 20.0),
-                ),
-                trailing: Text(
-                  electronicId,
-                  style: TextStyle(fontSize: 20.0),
-                ),
-              ),
-            );
-          });
-    } else {
-      return Center(
-          child: Text(
-        "Welcome. Your list is empty",
-        textAlign: TextAlign.center,
-        style: TextStyle(fontSize: 30.0),
-      ));
-    }
-  }
-
- */
-  Widget inserirAnimal(){
-    return new FlatButton(
-            onPressed: () {
-            Navigator.push(context, MaterialPageRoute(builder: (ctx) => NewAnimalPage()));
-          }
-          );
-  }
-
-  Widget buscarAnimal(){
-    return new FlatButton(
-        onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (ctx) => NewAnimalPage()));
-        }
-    );
-  }
-
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -170,13 +33,16 @@ class _HomePageState extends State<HomePage> {
           title: new Text('Cow Manager'),
           actions: <Widget>[
             new FlatButton(
-                child: new Text('Logout',
-                    style: new TextStyle(fontSize: 17.0, color: Colors.white)),
-                onPressed: signOut)
+                child: new Text(
+                    'Logout',
+                    style: new TextStyle(fontSize: 17.0, color: Colors.white)
+                ),
+                onPressed: signOut
+            )
           ],
         ),
         body: _showMenu(),
-        );
+    );
   }
 
   Widget _showMenu() {
@@ -185,29 +51,32 @@ class _HomePageState extends State<HomePage> {
           context: context,
           tiles: [
             ListTile(
-              title: Text('Inserir'),
+              title: Text('New Animal'),
               trailing: Icon(Icons.add),
               onTap: () {
                 Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                    builder: (context) => NewAnimalPage(),
-                ));
+                  context,
+                  MaterialPageRoute(
+                  builder: (context) => NewAnimalPage(),
+                  )
+                );
               }
             ),
             ListTile(
-              title: Text('Buscar animal'),
-              trailing: Icon(Icons.assignment),
+              title: Text('Search Animal'),
+              trailing: Icon(Icons.search),
                 onTap: () {
                   Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => NewAnimalPage(),
-                      ));
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SearchAnimalPage(),
+                    )
+                  );
                 }
             ),
           ]
       ).toList(),
     );
   }
+
 }
--- lib/view/search_animal.dart
+++ lib/view/search_animal.dart
@@ -1,17 +1,29 @@
+import 'package:cow_manager/chip_field/ChipFormField.dart';
 import 'package:flutter/material.dart';
-
 
 class SearchAnimalPage extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => new _SearchAnimalPageState();
 }
-  class _SearchAnimalPageState extends State<SearchAnimalPage> {
 
-    @override
-    Widget build(BuildContext context) {
-      return new Scaffold(
+class _SearchAnimalPageState extends State<SearchAnimalPage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Cow Manager'),
+        ),
+        body: _showSearch());
+  }
 
-      );
-    }
+  Widget _showSearch() {
+    // create a new ChipFormField widget to get an animal identifier
+    // use _chipFormField.chip to get the animal identifier whenever needed
+    //
+    ChipFormField _chipFormField = ChipFormField(context, 'HC-06', (chip) {
+      print('CHIP: ' + chip);
+    });
 
- }
+    return _chipFormField;
+  }
+}
